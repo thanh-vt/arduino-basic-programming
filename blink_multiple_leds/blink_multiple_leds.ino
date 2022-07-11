091fece886d417b69179342f9f5d6ee622ffd090
thanhvt <thanhvt@vissoft.vn>
2022-07-12 00:29:20 +0700

    add serial monitor print

--- blink_multiple_leds/blink_multiple_leds.ino
+++ blink_multiple_leds/blink_multiple_leds.ino
@@ -1,8 +1,9 @@
 #include <Arduino.h>
 
-byte ledPin[10] = {2, 4, 7, 8, 12}; // danh sách các chân điều khiển led đơn.
+byte ledPin[5] = {2, 4, 7, 8, 12}; // danh sách các chân điều khiển led đơn.
 
 void setup() {
+    Serial.begin(9600);
     // 5 = số phần tử của ledPin
     for (unsigned char x : ledPin) {
         pinMode(x, OUTPUT); // cấu hình các chân tron
@@ -10,14 +11,20 @@ void setup() {
     }
 }
 
+char str[80];
+
 void loop() {
     for (unsigned char x : ledPin) {
         digitalWrite(x, LOW); // xuất tín hiệu ra các chân điều khiển led là mức thấp
+        sprintf(str, "Pin low: %d", x);
+        Serial.println(str);
         delay(500);
     }
 
     for (unsigned char x : ledPin) {
         digitalWrite(x, HIGH); // xuất tín hiệu ra các chân điều khiển led là mức cao.
+        sprintf(str, "Pin high: %d", x);
+        Serial.println(str);
         delay(500);
     }
 
--- blink_multiple_leds/cmake-build-debug-mingw/blink_multiple_leds_blink_multiple_leds.ino.cpp
+++ blink_multiple_leds/cmake-build-debug-mingw/blink_multiple_leds_blink_multiple_leds.ino.cpp
@@ -14,9 +14,10 @@
 #line 1 "F:/Programming/C/arduino-basics/blink_multiple_leds/blink_multiple_leds.ino"
 
 
-byte ledPin[10] = {2, 4, 7, 8, 12}; // danh sách các chân điều khiển led đơn.
+byte ledPin[5] = {2, 4, 7, 8, 12}; // danh sách các chân điều khiển led đơn.
 
 void setup() {
+    Serial.begin(9600);
     // 5 = số phần tử của ledPin
     for (unsigned char x : ledPin) {
         pinMode(x, OUTPUT); // cấu hình các chân tron
@@ -24,14 +25,20 @@ void setup() {
     }
 }
 
+char str[80];
+
 void loop() {
     for (unsigned char x : ledPin) {
         digitalWrite(x, LOW); // xuất tín hiệu ra các chân điều khiển led là mức thấp
+        sprintf(str, "Pin low: %d", x);
+        Serial.println(str);
         delay(500);
     }
 
     for (unsigned char x : ledPin) {
         digitalWrite(x, HIGH); // xuất tín hiệu ra các chân điều khiển led là mức cao.
+        sprintf(str, "Pin high: %d", x);
+        Serial.println(str);
         delay(500);
     }
 
